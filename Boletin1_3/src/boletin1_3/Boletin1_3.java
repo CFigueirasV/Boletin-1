29d3ea0d340880efc4faf5459265d8f0dcdd4b6f
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-10-25 09:51:13 +0200

    Boletin1_2

---
+++ Boletin1_3/src/boletin1_3/Boletin1_3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_3;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin1_3 {
+public static void main(String[] args) {
+        float euro = 100, valorDolar = 1.16f;
+        System.out.println(euro+" euros son "+euro*valorDolar+" dolares");
+        
+        // TODO code application logic here
+    }
+    
+}
