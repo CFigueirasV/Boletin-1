2139b9a426b8bcd873f1d16a683a147fd117baf2
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-10-29 11:06:31 +0100

    Boletin1_1

---
+++ Boletin1_5/src/boletin1_5/Boletin1_5.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin1_5 {
+public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner (System.in);
+     float millasMarinas = 0, conversionM = 1852 ,conversor=0;
+     System.out.println("Introduzca las millas marinas: ");
+     millasMarinas = leer.nextFloat();
+     conversor=millasMarinas*conversionM;
+     System.out.println(+millasMarinas+" Millas marinas son " +conversor+ " metros");
+     
+     
+        
+        
+    }
+    
+}
